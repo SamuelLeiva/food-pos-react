a4994c13d4e684d08a39986a83441b83f6bbcce9
SamuelLeiva <leiva20152@gmail.com>
2025-09-22 13:16:13 -0500

    fix: isActive conditional added to MenuItemCard

--- src/components/MenuItemCard.tsx
+++ src/components/MenuItemCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import type { MenuItem } from '../features/menu/types/MenuItem';
+import React from "react";
+import type { MenuItem } from "../features/menu/types/MenuItem";
 
 type Props = {
   item: MenuItem;
@@ -8,30 +8,29 @@ type Props = {
 
 export const MenuItemCard: React.FC<Props> = ({ item, onSelect }) => {
   return (
-    <div
-      className="bg-white rounded-lg shadow-sm overflow-hidden cursor-pointer transition-all duration-200 hover:shadow-lg hover:-translate-y-1"
-      onClick={() => onSelect(item)}
-    >
-      <img
-        src={item.imageUrl}
-        alt={item.name}
-        className="w-full h-32 object-cover"
-      />
-      <div className="p-4">
-        <h3 className="text-md font-semibold text-gray-800 truncate">
-          {item.name}
-        </h3>
-        <div className="flex items-center gap-2 mt-2">
-          <span className="text-lg font-bold text-blue-600">
-            ${item.price.toFixed(2)}
-          </span>
-          {item.price && (
-            <span className="text-sm text-gray-400 line-through">
+    // si está activo, se renderiza la card
+    item.isActive && (
+      <div
+        className="bg-white rounded-lg shadow-sm overflow-hidden cursor-pointer transition-all duration-200 hover:shadow-lg hover:-translate-y-1"
+        onClick={() => onSelect(item)}
+      >
+        <img
+          src={item.imageUrl}
+          alt={item.name}
+          className="w-full h-32 object-cover"
+        />
+        <div className="p-4">
+          <h3 className="text-md font-semibold text-gray-800 truncate">
+            {item.name}
+          </h3>
+          <p>{item.description}</p>
+          <div className="flex items-center gap-2 mt-2">
+            <span className="text-lg font-bold text-blue-600">
               ${item.price.toFixed(2)}
             </span>
-          )}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
