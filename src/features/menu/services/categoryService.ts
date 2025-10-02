d865d700b4cc420ecf07eccf71d5b0c3278ba03a
SamuelLeiva <leiva20152@gmail.com>
2025-10-02 11:58:49 -0500

    fix: categoryService fixed

--- src/features/menu/services/categoryService.ts
+++ src/features/menu/services/categoryService.ts
@@ -6,5 +6,5 @@ import type { Category } from "../types/Category";
 export const fetchCategories = async () => {
   const response: AxiosResponse<ApiResponse<Category[]>>  = await apiClient.get('categories');
   const fullResponse = response.data
-  return fullResponse.data;
+  return fullResponse.data ?? [];
 };
